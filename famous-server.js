1234fda5288818f36bee46b6c6990dabf7e77f81
Morten Henriksen <mh@gi2.dk>
2014-04-14 01:04:04 +0200

    clean up a bit

--- famous-server.js
+++ famous-server.js
@@ -1,7 +1,3 @@
-//console.log(WebApp.clientProgram.manifest);
-
-console.log(__meteor_bootstrap__.serverDir);
-
 Famous = {
   require: function(dep) {
     throw new Error('Famous require cannot run on the server');
