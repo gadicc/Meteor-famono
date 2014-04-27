1fcbb0520282f6891af818c46e263402100a3a9a
Morten Henriksen <mh@gi2.dk>
2014-04-27 23:33:37 +0200

    npm cli to module

---
+++ famono.js
@@ -0,0 +1,67 @@
+#!/usr/bin/env node
+/*
+
+  We create jsdoc into gh-pages submodule in folder "docs" and auto push changes
+
+*/
+
+
+// CLI Options
+var program = require('commander');
+// CLI Colored text
+var colors = require('colors');
+// CLI Progress bar
+var ProgressBar = require('progress');
+// Use the famono lib
+var lib = require('./famono-lib');
+
+program
+  .version('0.0.3')
+  .option('-a, --add <name>', 'Add library')
+  .option('-d, --del <name>', 'Remove library')
+  .option('-l, --list', 'List of used libraries')
+  .option('-p, --path <path>', 'use git or local path')
+
+  .parse(process.argv);
+
+
+if (program.add) {
+
+  if (program.path) {
+
+    // Add the package
+    lib.setConfig(program.add, program.path);
+
+  } else {
+
+    getBowerData(program.add, function(err, result) {
+
+      if (err) {
+        console.log('Could not resolve package name "' + program.add + '", ' + err.message);
+      } else {
+        // Add the package
+        lib.setConfig(result.name, result.url);
+      }
+
+    });
+
+  }
+
+} else if (program.del) {
+
+  // Remove the package
+  lib.setConfig(program.del);
+
+} else if (program.list) {
+  var config = lib.loadConfig();
+  if (config) {
+    
+    console.log('Used libraries:');
+    
+    for (var name in config)
+      console.log('-', name);
+
+  } else {
+    console.log('Famono: Error, could not load smart.require');
+  }
+}
