a70ce81b86390d8686e80dc9b627c91894f4e837
Morten Henriksen <mh@gi2.dk>
2014-07-07 23:06:49 +0200

    add some comments

--- package.js
+++ package.js
@@ -6,31 +6,34 @@ Package._transitional_registerBuildPlugin({
   name: 'compileRequirejs',
   use: [],
   sources: [
-    'compile_requirejs.js'
+    'compile_requirejs.js' // The holy grail!!
   ],
   npmDependencies: {
-    'sync-exec': '0.3.2',
-    'famono': '0.0.9'
+    'sync-exec': '0.3.2', // exec sync - we could also just run in a fiber?
+    'famono': '0.0.9' // Our way of to require famono_lib.js
   }
 });
 
 Npm.depends({
-  send: '0.1.4',
-  useragent: "2.0.7"
+  send: '0.1.4', // Serving library files async / lazyloading
+  useragent: "2.0.7" // Ment for client specific bundles?
 });
 
 Package.on_use(function(api) {
   'use strict';
 
-  api.use('webapp', 'server');
-  api.use('reload', 'client');
-  api.use('routepolicy', 'server');
+  api.use('webapp', 'server');  // Used for serving files
+  api.use('reload', 'client'); // Not sure if we are using this?
+  api.use('routepolicy', 'server'); // Not sure if this is used...
 
   api.add_files([
+    // Just noop server-side api warning if used on server
     'requirejs_server.js',
+    // The library lazyloading server
     'requirejs_libraries_server.js'
   ], 'server');
 
+  // This is browser client side require / define api
   api.add_files('requirejs_client.js', 'client');
 
   api.export('define');
