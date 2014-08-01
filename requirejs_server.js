59a55a95411714a2005158932c19cd6d909cbb9b
Morten Henriksen <mh@gi2.dk>
2014-08-01 20:09:21 +0200

    Fix #58

--- requirejs_server.js
+++ requirejs_server.js
@@ -1,7 +1,22 @@
-require = function () {
+
+if (typeof global.require == 'undefined') global.require = function () {
   throw new Error('Famous "require" cannot run on the server');
 }
 
-define = function () {
+if (typeof global.define == 'undefined') global.define = function () {
   throw new Error('Famous "define" cannot run on the server');
+};
+
+Famono = {};
+
+Famono.define = function () {
+  throw new Error('Famous "Famono.define" cannot run on the server');
+};
+
+Famono.require = function () {
+  throw new Error('Famous "Famono.require" cannot run on the server');
+};
+
+Famono.scope = function () {
+  throw new Error('Famous "Famono.scope" cannot run on the server');
 };
