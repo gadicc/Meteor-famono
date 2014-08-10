04b7dc7559568a9a0913a87d6ba73caa1ffd801f
Morten Henriksen <mh@gi2.dk>
2014-08-10 16:43:45 +0200

    Revert changes to please Meteor 0.9.0

--- package.js
+++ package.js
@@ -1,5 +1,5 @@
 Package.describe({
-  version: '0.7.2',
+  version: '0.7.3',
   summary: 'Library bundler supports use of libraries like Famo.us, via git/bower and requireJS/commonJS/AMD/UMD'
 });
 
@@ -24,9 +24,9 @@ Npm.depends({
 Package.on_use(function(api) {
   'use strict';
 
-  api.use('webapp@1.0.0', 'server');  // Used for serving files
-  api.use('reload@1.0.0', 'client'); // Not sure if we are using this?
-  api.use('routepolicy@1.0.0', 'server'); // Not sure if this is used...
+  api.use('webapp', 'server');  // Used for serving files
+  api.use('reload', 'client'); // Not sure if we are using this?
+  api.use('routepolicy', 'server'); // Not sure if this is used...
 
   //api.versionsFrom && api.versionsFrom('METEOR-CORE@0.9.0-preview4');
 
